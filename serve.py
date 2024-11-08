84e6fad0cc5a1ae6b35f7697025a021078ef989c
L <luenwarneke@gmail.com>
2024-11-08 15:20:22 +1000

    update serve ip for docker

--- serve.py
+++ serve.py
@@ -35,4 +35,4 @@ def search_author_id(id):
         return jsonify({"error": "Author not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
